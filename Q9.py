3328cd092d088b872288ca4ac03a7213971da2b3
aquaclasher <69949345+aquaclasher@users.noreply.github.com>
2026-05-15 03:24:17 +0530

    Updates

--- Q9.py
+++ Q9.py
@@ -1,10 +1,21 @@
 '''Using alignment data, construct a phylogenetic tree and visualize it with Bio.Phylo. Label each
 branch with the sequence name.'''
 from Bio import Phylo
+from Bio.SeqRecord import SeqRecord
 from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor
 from Bio import AlignIO
+from Bio.Align import MultipleSeqAlignment
+from Bio.Seq import Seq
 
-alignment = AlignIO.read("input_sequences.fasta", "fasta")
+print("Creating dummy alignment...")
+align1 = SeqRecord(Seq("ATGCGTACGTA"), id="seq1")
+align2 = SeqRecord(Seq("ATGCGTACGTC"), id="seq2")
+align3 = SeqRecord(Seq("ATGCGTACGAG"), id="seq3")
+alignment = MultipleSeqAlignment([align1, align2, align3])
+AlignIO.write(alignment, "aligned_sequences.aln", "clustal")
+
+
+alignment = AlignIO.read("aligned_sequences.aln", "clustal")
 
 calculator = DistanceCalculator("identity")
 distance_matrix = calculator.get_distance(alignment)
